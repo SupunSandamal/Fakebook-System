9ac5041bfe27d416f3308833bfa32df8a1421e3e
SupunSandamal <sandamalsupun18@gmail.com>
2023-04-18 22:44:46 +0530

    navbar

--- src/components/Navigation/NavBar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NavBar() {
-  return (
-    <div>This is Navigation bar</div>
-  )
-}
--- src/pages/Home/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import NavBar from '../../components/Navigation/NavBar'
-
-export default function Home() {
-  return (
-    <NavBar/>
-  )
-}
- 

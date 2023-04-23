7609696825c3175dd8eb0bb47c611c430f8863e3
SupunSandamal <sandamalsupun18@gmail.com>
2023-04-23 22:43:29 +0530

    Open create Post center

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,6 +3,25 @@ import "./post.css"
 
 export default function Post() {
   return (
-    <div>Post</div>
+    <div className="post">
+        <div className="postContainer">
+            <div className="postTop">
+                <img src="/Images/Supun.jpg" alt="" className="postImage" />
+                <span className="postUserName">Supun Sandamal</span>
+                <span className="postTime">5 mins ago</span>
+            </div>
+            <div className="postCenter">
+                <div className="postCaption">
+                    hey..! Skill is power..! 
+                </div>
+                <img src="/Images/Supun.jpg" alt="" className="postedImage" />
+            </div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }

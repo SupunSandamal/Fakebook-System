d3b7990c95b103d22b88dc55c289e1c061599feb
SupunSandamal <sandamalsupun18@gmail.com>
2023-04-26 13:16:51 +0530

    Add sponser line

--- src/components/RightPane/RightPane.js
+++ src/components/RightPane/RightPane.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import "./rightPane.css"
+import React from "react";
+import "./rightPane.css";
 
 export default function RightPane() {
   return (
-    <div className='rightPaneBox'>RightPane</div>
-  )
+    <div className="rightPaneBox">
+      <div className="rightPaneContainer">
+        <div className="adContainer">
+          <span className="sponsered">Sponsored</span>
+          <img src="/Images/add.jpg" alt="" className="adImage" />
+        </div>
+      </div>
+    </div>
+  );
 }

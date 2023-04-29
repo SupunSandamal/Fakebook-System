6728b6998c947a4dd6154b4891a88f22ca772b5a
SupunSandamal <sandamalsupun18@gmail.com>
2023-04-29 21:06:37 +0530

    Right pane start Add

--- src/components/RightPane/RightPane.js
+++ src/components/RightPane/RightPane.js
@@ -8,6 +8,20 @@ export default function RightPane() {
         <div className="adContainer">
           <span className="sponsered">Sponsored</span>
           <img src="/Images/add.jpg" alt="" className="adImage" />
+          <span className="adText">Big sell delivery service We can deliver into your All products</span>
+        </div>
+        <div className="dirthdayContainer">
+          <img src="Images/birthday.png" alt="" className="birthdayImage" />
+          <span className="bithdayText"><b>Pasindu liyanage</b> and <b>2 others</b> Having birthday today....!</span>
+        </div>
+        <div className="friendList">
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
         </div>
       </div>
     </div>

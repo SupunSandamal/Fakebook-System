7a04d92d334c9750f9e1badfe87c2bdc75c6f806
SupunSandamal <sandamalsupun18@gmail.com>
2023-04-29 21:30:29 +0530

    Finish Fakebook page

--- src/components/RightPane/RightPane.js
+++ src/components/RightPane/RightPane.js
@@ -12,9 +12,89 @@ export default function RightPane() {
         </div>
         <div className="dirthdayContainer">
           <img src="Images/birthday.png" alt="" className="birthdayImage" />
-          <span className="bithdayText"><b>Pasindu liyanage</b> and <b>2 others</b> Having birthday today....!</span>
+          <span className="birthdayText"><b>Pasindu liyanage</b> and <b>2 others</b> Having birthday today....!</span>
         </div>
         <div className="friendList">
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
+          <li className="onlineFriend">
+            <div className="onlineFriendImageContainer">
+              <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="onlineFriendName">Shaloni Leao</span>
+          </li>
+
           <li className="onlineFriend">
             <div className="onlineFriendImageContainer">
               <img src="/Images/friend.jpeg" alt="" className="onlineFriendImg" />
